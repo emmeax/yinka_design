257e3e6abc7090d50852decea27bb3bdda348bbf
Abdul <abdulmusty03@outlook.com>
2025-10-15 20:46:42 +0100

    chore: home page and nav revamp

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily:{
-        neue: ['NeueMontreal', 'sans-serif'],
-        technor: ['"Technor"', 'sans-serif'],
-        syne: ['Syne', 'sans-serif'],
-      }
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#A22CFF",
+            },
+            fontFamily: {
+                neue: ["NeueMontreal", "sans-serif"],
+                technor: ['"Technor"', "sans-serif"],
+                syne: ["Syne", "sans-serif"],
+            },
+        },
     },
-  },
-  plugins: [],
-}
-
+    plugins: [],
+};
