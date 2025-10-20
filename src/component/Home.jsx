bec857417527f96c607c7236196868aa94a69942
Abdul <abdulmusty03@outlook.com>
2025-10-20 03:59:00 +0100

    style: add home and navbar animation

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,3 +1,4 @@
+import { motion, stagger } from "motion/react";
 import Profilepics from "../assets/Image/Profilepics.png";
 import Document from "./icons/document";
 import LinkedIn from "./icons/linkedin";
@@ -5,8 +6,17 @@ import LinkedIn from "./icons/linkedin";
 const Home = () => {
     return (
         <section className="w-contain relative pt-20 pb-[10rem] text-white flex lg:flex-row flex-col lg:items-start justify-center gap-[3.75rem] z-10">
-            <div className="lg:w-[55%] space-y-4 font-medium">
-                <h2 className="lg:text-8xl text-5xl inline-flex flex-col gap-2 font-technor leading-[60px]">
+            <motion.div
+                variants={containerVariants}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.2 }}
+                className="lg:w-[55%] space-y-4 font-medium"
+            >
+                <motion.h2
+                    variants={itemVariants}
+                    className="lg:text-8xl text-5xl inline-flex flex-col gap-2 font-technor leading-[60px]"
+                >
                     <span className="lg:inline hidden">
                         CREATIVE DIRECTOR & DESIGNER
                     </span>
@@ -14,8 +24,11 @@ const Home = () => {
                     <span className="lg:text-5xl text-3xl">
                         with 7+ years of experience
                     </span>
-                </h2>
-                <p className="lg:text-2xl text-xl inline-flex flex-col gap-4">
+                </motion.h2>
+                <motion.p
+                    variants={itemVariants}
+                    className="lg:text-2xl text-xl inline-flex flex-col gap-4"
+                >
                     <span>
                         I specialize in taking Ideas and Visions from obscurity
                         to execution with Problem solving skills, Technology and
@@ -28,32 +41,84 @@ const Home = () => {
                         achieving success and create High Conversion Designs
                         that Resonate with Target Audiences.
                     </span>
-                </p>
-            </div>
+                </motion.p>
+            </motion.div>
 
-            <div className="lg:w-[25%] flex flex-col gap-6">
-                <img
+            <motion.div
+                variants={containerVariants}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.2 }}
+                className="lg:w-[25%] flex flex-col gap-6"
+            >
+                <motion.img
+                    variants={imageClipPathVariants}
                     src={Profilepics}
                     alt=""
                     className="size-full object-cover"
                 />
-                <div className="flex items-center gap-2 *:w-full">
-                    <a
+                <motion.div
+                    variants={containerVariants}
+                    className="flex items-center gap-2 *:w-full"
+                >
+                    <motion.a
+                        variants={itemVariants}
                         href=""
                         className="flex items-center justify-center gap-2 h-12 px-2.5 bg-primary rounded-2xl font-medium"
                     >
                         Download CV <Document className="size-4" />
-                    </a>
-                    <a
+                    </motion.a>
+                    <motion.a
+                        variants={itemVariants}
                         href=""
                         className="flex items-center justify-center gap-2 h-12 px-2.5 border border-primary rounded-2xl font-medium"
                     >
                         Let's connect <LinkedIn className="size-4" />
-                    </a>
-                </div>
-            </div>
+                    </motion.a>
+                </motion.div>
+            </motion.div>
         </section>
     );
 };
 
 export default Home;
+
+const containerVariants = {
+    hidden: {
+        opacity: 0,
+        // transition: { when: "afterChildren" },
+    },
+    visible: {
+        opacity: 1,
+        transition: {
+            // when: "beforeChildren",
+            delayChildren: stagger(0.3),
+        },
+    },
+};
+
+const itemVariants = {
+    hidden: {
+        opacity: 0,
+        y: 150,
+    },
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 1.5,
+        },
+    },
+};
+
+const imageClipPathVariants = {
+    hidden: {
+        clipPath: "inset(100% 0 0 0)",
+    },
+    visible: {
+        clipPath: "inset(0% 0 0 0)",
+        transition: {
+            duration: 1.5,
+        },
+    },
+};
--- src/component/smooth-scroll.jsx
+++ src/component/smooth-scroll.jsx
@@ -1,8 +1,23 @@
 import { ReactLenis } from "lenis/react";
+import { cancelFrame, frame } from "motion/react";
+import { useEffect, useRef } from "react";
 
 const SmoothScroll = ({ children }) => {
+    const lenisRef = useRef(null);
+
+    useEffect(() => {
+        function update(data) {
+            const time = data.timestamp;
+            lenisRef.current?.lenis?.raf(time);
+        }
+
+        frame.update(update, true);
+
+        return () => cancelFrame(update);
+    }, []);
+
     return (
-        <ReactLenis root options={{ lerp: 0.05 }}>
+        <ReactLenis root options={{ lerp: 0.05 }} ref={lenisRef}>
             {children}
         </ReactLenis>
     );
